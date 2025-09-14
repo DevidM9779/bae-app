9b59c4423cbc46d5c398d097d33d036395faa610
DevidM9779 <ddavidmilan@hotmail.es>
2025-09-14 10:20:52 -0500

    UnlockPage quick tweaks pt2

--- src/pages/UnlockPage.tsx
+++ src/pages/UnlockPage.tsx
@@ -9,8 +9,6 @@ const UnlockPage = () => {
   });
   const [isFinished, setIsFinished] = useState(false);
 
-  const [isFinished, setIsFinished] = useState(false);
-
   useEffect(() => {
     // const targetDate = new Date('2024-09-26T00:00:00-05:00').getTime();
     const targetDate = new Date('2025-09-26T12:00:00-05:00').getTime();
